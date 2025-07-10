394386d84a7877e91d69370414e5eda0dda17b2c
Pedro Isaac Maisincho Paucar <imasincho@gmail.com>
2025-07-10 01:01:26 -0500

    three commit

---
+++ PruebaUP/src/main/java/ec/espe/edu/model/SoccerTeam.java
@@ -0,0 +1,94 @@
+
+package ec.espe.edu.model;
+
+/**
+ *
+ * @author 
+ */
+public class SoccerTeam {
+    
+    private int id;
+    private String name;
+    private float ExpensiveTeam;
+    private String MostExpensiveSigning;
+    private float TranferPrice;
+
+    public SoccerTeam(int id, String name, float ExpensiveTeam, String MostExpensiveSigning, float TranferPrice) {
+        this.id = id;
+        this.name = name;
+        this.ExpensiveTeam = ExpensiveTeam;
+        this.MostExpensiveSigning = MostExpensiveSigning;
+        this.TranferPrice = TranferPrice;
+    }
+
+    /**
+     * @return the id
+     */
+    public int getId() {
+        return id;
+    }
+
+    /**
+     * @param id the id to set
+     */
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    /**
+     * @return the name
+     */
+    public String getName() {
+        return name;
+    }
+
+    /**
+     * @param name the name to set
+     */
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    /**
+     * @return the ExpensiveTeam
+     */
+    public float getExpensiveTeam() {
+        return ExpensiveTeam;
+    }
+
+    /**
+     * @param ExpensiveTeam the ExpensiveTeam to set
+     */
+    public void setExpensiveTeam(float ExpensiveTeam) {
+        this.ExpensiveTeam = ExpensiveTeam;
+    }
+
+    /**
+     * @return the MostExpensiveSigning
+     */
+    public String getMostExpensiveSigning() {
+        return MostExpensiveSigning;
+    }
+
+    /**
+     * @param MostExpensiveSigning the MostExpensiveSigning to set
+     */
+    public void setMostExpensiveSigning(String MostExpensiveSigning) {
+        this.MostExpensiveSigning = MostExpensiveSigning;
+    }
+
+    /**
+     * @return the TranferPrice
+     */
+    public float getTranferPrice() {
+        return TranferPrice;
+    }
+
+    /**
+     * @param TranferPrice the TranferPrice to set
+     */
+    public void setTranferPrice(float TranferPrice) {
+        this.TranferPrice = TranferPrice;
+    }
+        
+    }
---
+++ PruebaUP/src/main/java/ec/espe/edu/utils/MongoConnection.java
@@ -0,0 +1,36 @@
+
+package ec.espe.edu.utils;
+
+import com.mongodb.client.MongoClient;
+import com.mongodb.client.MongoClients;
+import com.mongodb.client.MongoDatabase;
+
+/**
+ *
+ * @author 
+ */
+public class MongoConnection {
+     private static final String URI = "mongodb+srv://isaac:isaac@cluster0.xaitfht.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+    private static final String DATABASE_NAME = "Soccer";
+    
+    private static MongoClient mongoClient = null;
+
+    private MongoConnection() {
+      
+    }
+
+    public static MongoDatabase getDatabase() {
+        if (mongoClient == null) {
+            mongoClient = MongoClients.create(URI);
+        }
+        return mongoClient.getDatabase(DATABASE_NAME);
+    }
+
+    public static void closeConnection() {
+        if (mongoClient != null) {
+            mongoClient.close();
+            mongoClient = null;
+        }
+    }
+    
+    }
